0d6ced37f763dee26dd790dace21c5dd5858b765
anmaxwell <aniamaxwell@gmail.com>
2021-02-21 22:09:04 +0000

    creating web page using template

--- makerpage/routes.py
+++ makerpage/routes.py
@@ -2,10 +2,13 @@ from makerpage import app, db
 from makerpage.models import QuoteForm
 from makerpage.forms import QuotePage
 from flask import render_template, request, redirect, url_for, flash, json
+from random import randrange
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=['GET','POST'])
 def index():
 
     form = QuoteForm()
+    quoteno = randrange(10)
+    quotedb = QuoteForm.query.filter(QuoteForm.id == int(quoteno))
 
-    return render_template('quotepage.html', form=form)
+    return render_template('index.html', quotedb=quotedb, form=form)
